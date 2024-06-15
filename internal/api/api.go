87488d5defb2c5923a412bd54672c2817818fec5
Диков Артем Сергеевич <a.dikov@MAC2069.local>
2024-06-15 15:21:05 +0300

    Iter14

--- internal/api/api.go
+++ internal/api/api.go
@@ -14,8 +14,9 @@ type APIServer struct {
 	cfg  *config.ServerConfig
 	echo *echo.Echo
 	st   *storage.MemStorage
+    storageProvider storage.StorageWorker
 }
-
+/*
 func New() *APIServer {
 	apiS := &APIServer{}
 	cfg := config.NewServer()
@@ -66,6 +67,55 @@ func New() *APIServer {
 
 	return apiS
 }
+*/
+func New() *APIServer {
+	apiS := &APIServer{}
+	cfg := config.NewServer()
+	apiS.cfg = cfg
+	apiS.echo = echo.New()
+	apiS.st = storage.NewMem()
+
+	var err error
+	switch cfg.GetProvider() {
+	case storage.FileProvider:
+		apiS.storageProvider = storage.NewFileProvider(cfg.FilePath, cfg.StoreInterval, apiS.st)
+	case storage.DBProvider:
+		apiS.storageProvider, err = storage.NewDBProvider(cfg.DatabaseDSN, cfg.StoreInterval, apiS.st)
+	}
+	if err != nil {
+		zap.S().Error(err)
+	}
+	if cfg.Restore {
+		err := apiS.storageProvider.Restore()
+		if err != nil {
+			zap.S().Error(err)
+		}
+	}
+
+	if cfg.StoreIntervalNotZero() {
+		go apiS.storageProvider.IntervalDump()
+	}
+
+	apiS.echo.Use(middlewares.WithLogging())
+	apiS.echo.Use(middlewares.GzipUnpacking())
+	if cfg.SignPass != "" {
+		apiS.echo.Use(middlewares.CheckSignReq(cfg.SignPass))
+	}
+
+	return apiS
+}
+
+func (a *APIServer) RegisterRoutes(e *echo.Echo) {
+	handler := handlers.New(a.st)
+	e.GET("/", handler.AllMetricsValues())
+	e.POST("/value/", handler.GetValueJSON())
+	e.GET("/value/:typeM/:nameM", handler.MetricsValue())
+	e.POST("/update/", handler.UpdateJSON())
+	e.POST("/update/:typeM/:nameM/:valueM", handler.UpdateMetrics())
+	e.POST("/updates/", handler.UpdatesJSON())
+	e.GET("/ping", handler.PingDB(a.storageProvider))
+}
+
 
 func (a *APIServer) Start() error {
 	err := a.echo.Start(a.cfg.Addr)
