be9ad99bc434b2460a3fafea6ab6652c52f69bf2
Диков Артем Сергеевич <a.dikov@MAC2069.local>
2024-06-15 12:29:06 +0300

    Iter14

--- internal/handlers/hanglers.go
+++ internal/handlers/hanglers.go
@@ -1,16 +1,16 @@
 package handlers
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"github.com/labstack/echo/v4"
-	"github.com/lionslon/go-yapmetrics/internal/models"
-	"github.com/lionslon/go-yapmetrics/internal/storage"
-	"go.uber.org/zap"
-	"io"
-	"net/http"
-	"strconv"
+    "encoding/json"
+    "errors"
+    "fmt"
+    "github.com/labstack/echo/v4"
+    "github.com/lionslon/go-yapmetrics/internal/models"
+    "github.com/lionslon/go-yapmetrics/internal/storage"
+    "go.uber.org/zap"
+    "io"
+    "net/http"
+    "strconv"
 )
 
 type storageUpdater interface {
