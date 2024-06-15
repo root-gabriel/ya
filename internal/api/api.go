ba67713672d815d9686b6f97fd1d1a80164fbbd9
Диков Артем Сергеевич <a.dikov@MAC2069.local>
2024-06-15 14:48:35 +0300

    Iter14

--- internal/api/api.go
+++ internal/api/api.go
@@ -7,68 +7,14 @@ import (
 	"github.com/lionslon/go-yapmetrics/internal/middlewares"
 	"github.com/lionslon/go-yapmetrics/internal/storage"
 	"go.uber.org/zap"
-	"log"
 )
 
 type APIServer struct {
-	Cfg  *config.ServerConfig
-	echo *echo.Echo
-	st   *storage.MemStorage
-    storageProvider storage.StorageWorker //NEW
+	Cfg             *config.ServerConfig
+	echo            *echo.Echo
+	st              *storage.MemStorage
+	storageProvider storage.StorageWorker
 }
-
-/*func New() *APIServer {
-	apiS := &APIServer{}
-	cfg := config.NewServer()
-	apiS.cfg = cfg
-	apiS.echo = echo.New()
-	apiS.st = storage.NewMem()
-
-	handler := handlers.New(apiS.st)
-	logger, _ := zap.NewDevelopment()
-	zap.ReplaceGlobals(logger)
-	defer logger.Sync()
-
-	var storageProvider storage.StorageWorker
-	var err error
-	switch cfg.GetProvider() {
-	case storage.FileProvider:
-		storageProvider = storage.NewFileProvider(cfg.FilePath, cfg.StoreInterval, apiS.st)
-	case storage.DBProvider:
-		storageProvider, err = storage.NewDBProvider(cfg.DatabaseDSN, cfg.StoreInterval, apiS.st)
-	}
-	if err != nil {
-		zap.S().Error(err)
-	}
-	if cfg.Restore {
-		err := storageProvider.Restore()
-		if err != nil {
-			zap.S().Error(err)
-		}
-	}
-
-	if cfg.StoreIntervalNotZero() {
-		go storageProvider.IntervalDump()
-	}
-
-    apiS.echo.Use(middleware.Gzip()) //NEW
-	apiS.echo.Use(middlewares.WithLogging())
-	apiS.echo.Use(middlewares.GzipUnpacking())
-	if cfg.SignPass != "" {
-		apiS.echo.Use(middlewares.CheckSignReq(cfg.SignPass))
-	}
-
-	apiS.echo.GET("/", handler.AllMetricsValues())
-	apiS.echo.POST("/value/", handler.GetValueJSON())
-	apiS.echo.GET("/value/:typeM/:nameM", handler.MetricsValue())
-	apiS.echo.POST("/update/", handler.UpdateJSON())
-	apiS.echo.POST("/update/:typeM/:nameM/:valueM", handler.UpdateMetrics())
-	apiS.echo.POST("/updates/", handler.UpdatesJSON())
-	apiS.echo.GET("/ping", handler.PingDB(storageProvider))
-
-	return apiS
-}*/
-
 
 func New() *APIServer {
 	apiS := &APIServer{}
@@ -107,7 +53,6 @@ func New() *APIServer {
 	return apiS
 }
 
-//NEW
 func (a *APIServer) RegisterRoutes(e *echo.Echo) {
 	handler := handlers.New(a.st)
 	e.GET("/", handler.AllMetricsValues())
@@ -120,10 +65,11 @@ func (a *APIServer) RegisterRoutes(e *echo.Echo) {
 }
 
 func (a *APIServer) Start() error {
-	err := a.echo.Start(a.cfg.Addr)
+	err := a.echo.Start(a.Cfg.Addr)
 	if err != nil {
-		log.Fatal(err)
+		zap.S().Fatal(err)
 	}
 
 	return nil
 }
+
