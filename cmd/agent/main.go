cf3f74f2d5593f9c55b27c68dd9da4e7cee5edd2
root-gabriel <dikof.f@yandex.ru>
2024-06-16 13:59:35 +0300

    TEST

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -8,10 +8,14 @@ import (
 
 func main() {
 	for {
-		_, err := http.Get("http://localhost:8080/update/counter/testCounter/1")
+		resp, err := http.Get("http://localhost:8080/update/counter/testCounter/1")
 		if err != nil {
 			log.Println("Error updating counter:", err)
+			time.Sleep(10 * time.Second)
+			continue
 		}
+		resp.Body.Close()  // Закрытие тела ответа
 		time.Sleep(10 * time.Second)
 	}
 }
+
